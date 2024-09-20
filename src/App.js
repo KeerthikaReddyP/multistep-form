e501be30c364e2f76afeebfac83c3e2f28239990
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-09-20 12:41:58 +0530

    Made it multi-step

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import Step1 from "./components/Step1";
 import Step2 from "./components/Step2";
 import Step3 from "./components/Step3";
 
 function App() {
+
+  const [step,setStep]=useState(1);
+
   return (
     <div className="m-4 p-2 bg-amber-600">
-      <Step1 />
-      <Step2 />
-      <Step3 />
+      {step===1 && <Step1 />}
+      {step===2 && <Step2 />}
+      {step===3 && <Step3 />}
     </div>
   );
 }
