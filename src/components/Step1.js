cf32fdd85315fefb92a76fabd0220de9f119bebe
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-09-20 17:12:02 +0530

    Adding user entered data to formData

--- src/components/Step1.js
+++ src/components/Step1.js
@@ -1,18 +1,23 @@
 const Step1=({formData, setFormData})=>{
+
+    const handleChange=(e)=>{
+        const {key,value}=e.target;
+        setFormData((prevData)=>({...prevData,[key]:value}));
+    };
     
     return <div>
         <h2>Personal details</h2>
         <div>
             <label>Name : </label>
-            <input type="text" name="name"/>
+            <input type="text" name="name" value={formData.name} onChange={handleChange}/>
         </div>
         <div>
             <label>Email : </label>
-            <input type="email" name="email"/>
+            <input type="email" name="email" value={formData.email} onChange={handleChange}/>
         </div>
         <div>
             <label>Mobile no. : </label>
-            <input type="text" name="mobile"/>
+            <input type="text" name="mobile" value={formData.mobile} onChange={handleChange}/>
         </div>
     </div>
 }
--- src/components/Step2.js
+++ src/components/Step2.js
@@ -1,26 +1,31 @@
 const Step2=({formData,setFormData})=>{
-    
+
+    const handleChange=(e)=>{
+        const {key,value}=e.target;
+        setFormData((prevData)=>({...prevData,[key]:value}));
+    };
+
     return <div>
         <h2>Address</h2>
         <div>
             <label>Address line-1 :</label>
-            <input type="text" name="address1"/>
+            <input type="text" name="address1" value={formData.address1} onChange={handleChange}/>
         </div>
         <div>
             <label>Address line-2 :</label>
-            <input type="text" name="address2"/>
+            <input type="text" name="address2" value={formData.address2} onChange={handleChange}/>
         </div>
         <div>
             <label>City :</label>
-            <input type="text" name="city" />
+            <input type="text" name="city" value={formData.city} onChange={handleChange}/>
         </div>
         <div>
             <label>State :</label>
-            <input type="text" name="state" />
+            <input type="text" name="state" value={formData.state} onChange={handleChange}/>
         </div>
         <div>
             <label>PIN code :</label>
-            <input type="text" name="pin" />
+            <input type="text" name="pincode" value={formData.pincode} onChange={handleChange}/>
         </div>
     </div>
 }
